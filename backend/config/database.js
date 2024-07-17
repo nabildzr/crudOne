56bf618713b137705ecbba925b2cb6e3eaa8db86
nabildzr <65263479+nabildzr@users.noreply.github.com>
2024-07-17 21:40:43 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('crud_db', 'root', '', {
+const db = new Sequelize('biyogsbecxqwhtyxlmcu', 'root', '', {
     host: 'localhost',
     dialect: 'mysql'
 })
